1d0c88b199b8aee2672fe93ab873f3c1cc75ac58
Jtensetti <123409055+Jtensetti@users.noreply.github.com>
2024-01-19 11:20:14 +0100

    Create AdminDashboard.js

---
+++ frontend/src/components/AdminDashboard.js
@@ -0,0 +1,21 @@
+// AdminDashboard.js (i frontend/src/components)
+
+import React from 'react';
+
+function AdminDashboard() {
+  return (
+    <div>
+      <h1>Lärare Dashboard</h1>
+      <section>
+        <h2>Klassrumsplanering</h2>
+        {/* Komponent för att hantera klassrumsplanering */}
+      </section>
+      <section>
+        <h2>Elevuppgifter</h2>
+        {/* Komponent för att granska och betygsätta elevuppgifter */}
+      </section>
+    </div>
+  );
+}
+
+export default AdminDashboard;
