fe400c6c7bccdad9911aeb3aae18131645dea905
Jtensetti <123409055+Jtensetti@users.noreply.github.com>
2024-01-19 11:18:54 +0100

    Create StudentDashboard.js

---
+++ frontend/src/components/StudentDashboard.js
@@ -0,0 +1,21 @@
+// StudentDashboard.js (i frontend/src/components)
+
+import React from 'react';
+
+function StudentDashboard() {
+  return (
+    <div>
+      <h1>Elevskrivbord</h1>
+      <section>
+        <h2>Dagens Schema</h2>
+        {/* Komponent för att visa dagens schema */}
+      </section>
+      <section>
+        <h2>Läromaterial och Uppgifter</h2>
+        {/* Komponent för att visa och hantera inlämningsuppgifter */}
+      </section>
+    </div>
+  );
+}
+
+export default StudentDashboard;
